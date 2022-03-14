30da3c4a370b4961e8564ef7a8df98617e01919c
ambiti0n <fabulousfresh010@gmail.com>
2022-03-14 09:17:30 +0100

    function to initialize our account added

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -7,8 +7,23 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod myepicproject {
   use super::*;
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+      // Get a reference to the account.
+      let base_account = &mut ctx.accounts.base_account;
+      // Initialize total_gifs.
+      base_account.total_gifs = 0;
     Ok(())
   }
+}
+
+
+// Attach certain variables to the StartStuffOff context.
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
 }
 
 #[derive(Accounts)]
